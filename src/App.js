34a09b054d5e60066876b9661269f870527a6591
Rhuan Santos <50742592+rhuangabrielsantos@users.noreply.github.com>
2021-06-13 11:40:35 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App" class="flex items-center justify-center bg-black w-screen h-screen">
+        <h1 class="text-white text-3xl">boilerplate tailwind with react.js</h1>
+    </div>
+  );
+}
+
+export default App;
