16a126fdefcc6799bcb63d9b10e3dc5970df6e03
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-07-27 19:51:32 -0300

    :sparkles: Added hover in button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ import Logo from "../assets/logo.svg";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center bg-nosferatu h-screen w-screen">
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center mb-36 md:mb-24">
         <img src={Logo} alt="Logo" className="w-20 h-auto m-3 md:w-48 md:m-8" />
         <h1 className="font-righteous text-cullen text-4xl md:text-7xl">
           Memory Game
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
 
       <Link to="/game">
-        <button className="bg-marcelin w-48 h-14 mt-36 font-righteous text-cullen text-2xl rounded-xl md:mt-24 md:w-80 md:h-20 md:text-4xl">
+        <button className="bg-marcelin hover:bg-opacity-75 duration-300 w-48 h-14 font-righteous text-cullen text-2xl rounded-xl md:w-80 md:h-20 md:text-4xl">
           Play
         </button>
       </Link>
