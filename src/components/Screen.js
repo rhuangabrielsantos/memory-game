8197db09fe069648f1c89d641d11bf304141369d
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-09-04 15:37:28 -0300

    :dizzy: Add animation in pages

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Screen(props) {
   return (
-    <div className="flex flex-col items-center justify-center bg-nosferatu h-screen w-screen">
+    <motion.div
+      initial={{ opacity: 0, y: +70 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="flex flex-col items-center justify-center bg-nosferatu h-screen w-screen"
+    >
       {props.children}
-    </div>
+    </motion.div>
   );
 }
