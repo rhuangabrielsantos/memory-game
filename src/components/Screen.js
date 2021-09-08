33997f4498292efb7f31d17a5a2b9346846b00d3
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-09-08 08:55:45 -0300

    :bug: Fix scroll

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,13 +3,15 @@ import { motion } from "framer-motion";
 
 export default function Screen(props) {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: +70 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      className="flex flex-col items-center justify-center bg-nosferatu h-screen w-screen"
-    >
-      {props.children}
-    </motion.div>
+    <div className="overflow-hidden">
+      <motion.div
+        initial={{ opacity: 0, y: +30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="flex flex-col items-center justify-center bg-nosferatu h-screen w-screen"
+      >
+        {props.children}
+      </motion.div>
+    </div>
   );
 }
